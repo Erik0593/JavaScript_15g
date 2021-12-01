9d0754190c1d9a7e1eedf8df3e741d56c6616121
Erik Gutierrez <gutierrez_m_e@hotmail.com>
2021-11-30 22:30:07 -0600

    clase 6

--- assets/js/base.js
+++
@@ -1,9 +0,0 @@
-const multiplo = (a=3, b=5) =>{
-  let mult = 0
-  for(i=1; i<=100;i++){
-      if(i % a === 0 || i % b === 0){
-          mult += i
-      }
-  }
-  return(`La suma de los multiplos es = ${mult}`)
-}
--- assets/js/exercise.js
+++ assets/js/exercise.js
@@ -2,9 +2,9 @@
 // valor por defecto 10
 // Imprimir en consola las tablas de multiplicar del 1 al 10 
 
-function printMultiTables( tableLimit = 10) {
-    for(i=0; i<= tableLimit; i++){
-        for(j=0; j<= 10; j++){
+const tables = ( tableLimit = 10) => {
+    for(i=1; i<= tableLimit; i++){
+        for(j=1; j<= 10; j++){
             console.log(`${i} x ${j} = ${i * j}`)
         }
     }
@@ -16,18 +16,15 @@ function printMultiTables( tableLimit = 10) {
 // valor por defecto 10
 // Imprimir en consola la suma de 1 a N, siendo N un numero entre 1 y 100
 
-function addNumbersRange(numberEnd = 10) {
+const addNumbersRange = (numberEnd = 10) => {
     let addRange = 0
-    if(!isNaN(parseInt(numberEnd)) || numberEnd >= 1 && numberEnd <= 100){
+    if (!isNaN(parseInt(nuberEnd)) || numberEnd >=1 && numberEnd <=100){
         for(i = 1; i <= numberEnd; i++){
             addRange += i
         }
-    } else {
-        console.log('No es un número valido')
     }
-
-    return addRange
-}
+        return addRange
+  }
 
 
 // Función 3:
@@ -36,7 +33,7 @@ function addNumbersRange(numberEnd = 10) {
 // p.ej. "1234" -> 10
 
 
-function sumatotal (valor = 1234) {
+const sumatotal = (valor = 1234) => {
     let anterior = valor.toString()
     let nuevo = 0
     for(i = 0; i < anterior.length; i++){
