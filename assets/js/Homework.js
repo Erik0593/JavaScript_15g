31979e30cf0852e0a6c6881f3c1fa3284c3c1e13
Erik Gutierrez <gutierrez_m_e@hotmail.com>
2021-12-08 17:47:09 -0600

    homework

---
+++ assets/js/Homework.js
@@ -0,0 +1,123 @@
+/**
+ * Ejercicio 1.
+ * Dado un objeto inicial, hacer los siguientes puntos 
+ * 
+ * 1. Agregar el lenguaje 'Go' a la lista de lenguajes
+ * 2. Cambiar el nivel a 4  listo
+ * 3. Eliminar la propiedad avatar   listo
+ * 4. Agregar una nueva propiedad de edad y poner el valor de 30  listo
+ * 5. Imprimir en consola todos los lenguajes dominados  listo
+ * 6. Clonar el objeto en uno nuevo   listo
+ * 7. Imprimir en consola el nuevo objeto   listo
+ * 
+ * @hint https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
+ */
+
+ let koder = {
+    languages: ["JavaScript", "Python", "Ruby"],
+    isMentor: true,
+    level: 3,
+    avatar: "https://picsum.photos/200/300"
+}
+
+
+//agregar el valor de 'Go' dentro del array Languages 
+koder.languages.push("Go")
+console.log(koder)
+
+//cambiar el nivel a 4
+koder.level = 4 
+
+//borrar la llave avatar
+delete koder.avatar
+
+
+//agregar la llave age con el valor de 30
+let koderage = {
+    age: 30
+}
+
+Object.assign(koder,koderage)
+console.log(koder)
+
+//imprimir solo los lenguajes 
+console.log(koder.languages)
+
+//clonar un objeto 
+const koderclon = Object.assign({}, koder)
+
+//imprimir el nuevo objeto
+console.log(koderclon)
+
+
+/**
+ * Ejercicio 2:
+ * Escribir una funcion que reciba un parametro
+ * Verificar si el parametro es un objeto
+ * o si es un array
+ * p.ej.  
+ * ·> isAnObjectOrArray( [1,2,3] )
+ * -> 'Es un array'
+ * ·> isAnObjectOrArray( {key:'value'} )
+ * -> 'Es un Objeto'
+ * ·> isAnObjectOrArray( 'Hola mundo' )
+ * -> 'No es un array ni un objeto'
+ * 
+ * 
+ * @params {Object}, [Array] ,'string' - Estructura que se quiere validar
+ * @return 'String' - mensaje de respuesta
+ * 
+ * @hint https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof
+ * @hint https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/isArray
+ * 
+ */
+
+ const isAnObjectOrArray = (element) => {
+    if(Array.isArray(element)){
+        return 'Es un array'
+    }else if(typeof element === 'object'){
+        return 'Es un objeto'
+    }else{
+        return 'No es un array ni un objeto'
+    }
+}
+
+/**
+ * Ejercicio 3.
+ * Realizar una funcion que tome como parametro un objeto
+ * y devuelva un array de arrays con la siguiente estructura
+ * [ [key, value], [key, value] ]
+ * Resultado esperado: makePairs({ a: 1, b: 2 }) => [['a', 1], ['b', 2]]
+ * 
+ * @params {Object} object - El objeto que queremos transformar
+ * @return [Array] - El array que se espera retornar
+ * 
+ * @hint https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
+ * @hint https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+ * 
+ * Se tiene que realizar 2 soluciones, 1 con Object.entries() y otra con .map()
+ */
+const makePairs = (object) => {
+    
+}
+
+
+/**
+ * Ejercicio 4:
+ * Realizar una funcion que tome como parametro un string
+ * y retorne un array con todos caracteres del string
+ * 
+ * Resultado esperado: 
+ * ·> splitString('hola mundo')
+ * ·> ['h','o','l','a',' ','m','u','n','d','o']
+ * 
+ * @params 'String' - El String que queremos transformar
+ * @return [Array] - El array que se espera retornar
+ * 
+ * 
+ * @hint https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
+ */
+
+const splitString = (str) => {
+
+}
--- assets/js/base.js
+++ assets/js/base.js
@@ -1,181 +1,23 @@
-// // Propiedades
-// // key: valor
-// let carObject = {
-//     modelo: 'jetta',
-//     marca: 'VW',
-//     color: 'rojo',
-//     year: 2020,
-//     tenencias: [2019, 2020, 2021]
-// }
-
-// // console.log(carObject)
-// // console.log(carObject.tenencias[0])
-
-// // Objeto de Koder
-// let koder = {
-//     name: 'Erik',
-//     lastName: 'Gutierrez',
-//     age: 28,
-//     modulos: ['js', 'css'],
-//     generacion: 15
-// }
-
-
-// // ejercicio 2
-// // 1. name : jorge luis
-// // 2. lastName : Camarillo
-// // ... 
-
-// // for in
-// let counter = 1
-
-// for(let propiedad in koder) {
-//     console.log(`${counter}. ${propiedad} : ${koder[propiedad]}`)
-//     // counter = counter + 1
-//     // counter += 1     existen 3 formas aumentar el valor de un for
-//     counter++
-// }
-
-//ejercicio 3
-//Del siguiente objeto
-//1. contar el numeor de empleados
-//2. sumar total a pagar
-// -> son   3 empleados y el total es 130000
-
-
-// let salarios = {
-//     juan: 30000,
-//     albert: 50000,
-//     jorge:  50000,
-// }
-
-// let sal = 0
-// let empl = 0
-
-// for(let total in salarios){
-//     empl++
-//     sal++
-// }
-
-// console.log(`Son ${empl} empleados y el total es ${sal}`)
-
-//ejercicio 4
-
-// let objOnlyString = {
-//     modelo: 'jetta',
-//     marca: 'VW',
-//     color: 'rojo',
-//     year: 2020,
-//     tenencias: [2019, 2020, 2021],
-//     version: 'sport',
-//     origen: 'México'
-// }
-/*
-
-crear un arrow function
-*Recibir un objeto
-*devolver un array con todas las propiedades que son un string
-*Entrada: objOnlyString
-Salida: -> [modelo, marca, color, version, origen]
-
-*/
-
-// 1. Declarar una funcion que reciba un objeto
-// 2. Declarar un array vacio
-// 3. iterar el objeto ( for in )
-// 4. -- En cada iteracion 
-// 5. ----- Obtener el valor de cada llave
-// 6. ----- Verificar si es un string
-// 7. ----- sí si, Agregar al array
-// 8. -- fin iteracion
-// 9. retornar el array
-
-
-// const filterStringObjetc = (obj) => {
-//     let string = []
-//     for(key in obj){
-//         if(typeof obj[key] === 'string'){
-//             string.push(key)
-//         }
-//     }
-//     return string
-// }
-
-// let objOnlyString = {
-//     modelo: 'jetta',
-//     marca: 'VW',
-//     color: 'rojo',
-//     year: 2020,
-//     tenencias: [2019, 2020, 2021],
-//     version: 'sport',
-//     origen: 'México'
-// }
-
-// console.log(filterStringObjetc(objOnlyString))
-
-// Array de objetos
-// let koders =  [
-//     {
-//         name: 'jorge luis',
-//         lastName: 'Camarillo',
-//         age: 30,
-//         generation: 6,
-//         modulos: ['js','node js', 'cloud'],
-//     },
-//     {
-//         name: 'Erik',
-//         lastName: 'Gutierrez',
-//         age: 20,
-//         generation: 15,
-//         modulos: ['js'],
-//     },
-//     {
-//         name: 'Sara',
-//         lastName: 'Reveles',
-//         age: 24,
-//         generation: 12,
-//         modulos: ['js'],
-//     }
-// ]
-
-// koders.forEach((element, index, array) => {
-//     // console.log(element.lastName)
-//     // console.log(element.age)
-//     // console.log(element.generation)
-//     // console.log(element.modulos)
-//     // console.log(`${element.name} ${element.lastName} tiene ${element.age} años`)  
-// })
-
 /**
- * 1. Hacer una funcion que reciba un objeto de koders
- * 2. Obtener la suma de todas las edades
- * 3. Retornar el total
- * hint: .forEach() .reduce()
+ * Ejercicio 3.
+ * Realizar una funcion que tome como parametro un objeto
+ * y devuelva un array de arrays con la siguiente estructura
+ * [ [key, value], [key, value] ]
+ * Resultado esperado: makePairs({ a: 1, b: 2 }) => [['a', 1], ['b', 2]]
+ * 
+ * @params {Object} object - El objeto que queremos transformar
+ * @return [Array] - El array que se espera retornar
+ * 
+ * @hint https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
+ * @hint https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+ * 
+ * Se tiene que realizar 2 soluciones, 1 con Object.entries() y otra con .map()
  */
 
-//Psuedocodigo
-//1. tenemos que hacer la funcion de forEach
-//2. declarar una variable vacia para el total de la suma de las edades
-//3. acceder a los valores de age
-//4. sumar los valores de cada objeto 
-//5. retornar a la variable 
-//6. console.log(variable)
-
-let totalAge = 0
-
-koders.forEach((element) => { return totalAge += element.age})
-console.log(`La suma de las edades es ${totalAge}`)
 
 
-//Solucion Briss
-//funcion con foreach
-//acceder el valor de la edad
-// return la suma
-// let suma = 0
-// koders.forEach((element, index) => {
-// 	console.log(element)
-// 	console.log(index)
-// 	//console.log(`${element.age} total de años`)
-// 	return suma += element.age
-// });
-// console.log(suma)
+const makePairs = (object) => {
+    
+}
+
+
